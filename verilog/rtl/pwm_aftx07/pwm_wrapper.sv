c8a138fdf712acb28909256aa0d4c96d7258db76
Rafael <rmontei@purdue.edu>
2024-10-09 11:55:23 -0400

    pwm part2

--- verilog/rtl/pwm_aftx07/pwm_wrapper.sv
+++ verilog/rtl/pwm_aftx07/pwm_wrapper.sv
@@ -1,51 +1,32 @@
-module pwm_wrapper#(
-    parameter NUM_CHANNELS = 2   
+module pwm_wrapper #(
+    parameter NUM_CHANNELS = 2,
+    parameter ADDR_WIDTH = 32,
+    parameter DATA_WIDTH = 32
 )(
-    input CLK,
-    input nRST,
+    input logic CLK, nRST,
+    input logic wen, ren,
+    input logic [(DATA_WIDTH/8)-1 : 0] strobe,
+    input logic [DATA_WIDTH-1 : 0] addr, wdata,
+    output logic [DATA_WIDTH-1 : 0] rdata,
     output logic [NUM_CHANNELS-1:0] pwm_out,
-    bus_protocol_if.peripheral_vital busif
+    output logic error, request_stall
 );
-    localparam int NUM_REG_PER_CHAN = 3;
-    localparam int NUM_REGS = NUM_REG_PER_CHAN * NUM_CHANNELS;
+    // Instantiate the interface
+    bus_protocol_if #(.ADDR_WIDTH(ADDR_WIDTH), .DATA_WIDTH(DATA_WIDTH)) busif ();
 
-    localparam PERIOD_IND = 0;
-    localparam DUTY_IND = 1;
-    localparam COUNT_IND = 2;
+    // Instantiate PWM
+    pwm myPWM (CLK, nRST, pwm_out, busif);
 
-    genvar i;
+    // Assign connections
+    assign busif.wen = wen;
+    assign busif.ren = ren;
+    assign busif.addr = addr;
+    assign busif.wdata = wdata;
+    assign busif.strobe = strobe;
 
-    logic [NUM_REGS-1:0] addr_match;
-    logic [NUM_REGS-1:0] wen;
-    logic [NUM_REGS-1:0] ren;
+    assign rdata = busif.rdata;
+    assign error = busif.error;
+    assign request_stall = busif.request_stall;
 
-    // Write only module
-    assign busif.error = 1'b0;
-    assign busif.rdata = 1'b0;
-    assign busif.request_stall = 1'b0;
-
-    generate
-        for(i = 0; i < NUM_REGS; i++) begin : g_wen_ren
-            assign addr_match[i] = (busif.addr == i*4);
-            assign wen[i] = (busif.wen && addr_match[i]);
-            assign ren[i] = (busif.ren && addr_match[i]);
-        end
-    endgenerate
-
-    generate
-        for(i = 0; i < NUM_CHANNELS; i++) begin : g_pwm_channel
-            pwmchannel CHANNEL(
-                .clk(CLK),
-                .n_rst(nRST),
-                .pwm_out(pwm_out[i]),
-                .cont_wen(wen[(i*NUM_REG_PER_CHAN) + COUNT_IND]),
-                .period_wen(wen[(i*NUM_REG_PER_CHAN) + PERIOD_IND]),
-                .duty_wen(wen[(i*NUM_REG_PER_CHAN) + DUTY_IND]),
-                .duty_in(busif.wdata),
-                .period_in(busif.wdata),
-                .control_in(busif.wdata[2:0])
-            );
-        end
-    endgenerate
 
 endmodule
